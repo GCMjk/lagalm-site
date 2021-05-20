93f1e223c3e0cbd1a54cd70e4097de6605f797d6
Martin <martingarnica.c@outlook.com>
2021-05-20 14:57:00 -0500

    services page and scss

--- src/app/components/lagservices/lagservices.component.ts
+++ src/app/components/lagservices/lagservices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowRight, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-lagservices',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lagservices.component.scss']
 })
 export class LagservicesComponent implements OnInit {
+
+  faArrowRight = faArrowRight;
+  faArrowAltCircleRight = faArrowAltCircleRight;
 
   constructor() { }
 

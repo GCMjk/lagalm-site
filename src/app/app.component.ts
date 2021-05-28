59ab36497af39b410b528ade5a5768fb7fd96c33
Martin <martingarnica.c@outlook.com>
2021-05-28 15:32:17 -0500

    scroll top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -6,13 +10,27 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'lagalm-site';
+  subscription: Subscription;
 
   constructor(
-    private translate: TranslateService
+    private translate: TranslateService,
+    private router: Router
   ) {
     this.translate.setDefaultLang('es');
     this.translate.use('es');
+  }
+
+  ngOnInit(){
+    this.subscription = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
+    .subscribe(() => window.scrollTo(0, 0));
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
